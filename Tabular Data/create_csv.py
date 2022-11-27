c969dee33bddddf7b662e8829077b0249bccb852
Lisa Schmierer <lisa.schmierer@tum.de>
2022-11-27 11:03:01 +0100

    correct sorting by date

--- Tabular Data/create_csv.py
+++ Tabular Data/create_csv.py
@@ -12,24 +12,23 @@ def writeTables():
 
     for mode in ['train', 'val', 'test']:
         list_ids = dict_split[mode]
-        #index, EXAMDATE, DX, CDRSB, MMSE, LDELTOTAL, mPACCdigit, mPACCtrailsB, APOE4, FAQ, Month
         df_tabular = pd.DataFrame(columns=['ID', 'ses', 'cdrsb', 'ldeltotal', 'mpaccdigit', 'mpacctrailsb', 'mmse', 'label'])
+
         for subject in list_ids:
             rows_df = adni_merged.loc[adni_merged['index'] == subject]
-
+            #sort rows by examination date
+            rows_df['EXAMDATE'] = pd.to_datetime(rows_df['EXAMDATE'], format='%d/%m/%Y')
+            rows_df.sort_values('EXAMDATE', inplace= True)
             i = 0
             while i < rows_df.shape[0]-1:
-                session = rows_df['EXAMDATE'].iloc[i]
-                next_session = rows_df['EXAMDATE'].iloc[i+1]
-
-                d1 = datetime.strptime(session, "%d/%m/%Y")
-                d2 = datetime.strptime(next_session, "%d/%m/%Y")
+                current_ses = rows_df['EXAMDATE'].iloc[i]
+                next_ses = rows_df['EXAMDATE'].iloc[i+1]
 
                 #if next session is within 6 months, it is considered 1 examination -> skip results from next session
-                if (d2.year - d1.year) * 12 + d2.month - d1.month < 6:
+                if (next_ses.year - current_ses.year) * 12 + next_ses.month - current_ses.month < 6:
                     i += 1
 
-                new_row = {'ID': subject, 'ses': session, 'cdrsb': rows_df['CDRSB'].iloc[i] ,
+                new_row = {'ID': subject, 'ses': current_ses, 'cdrsb': rows_df['CDRSB'].iloc[i] ,
                            'ldeltotal':rows_df['LDELTOTAL'].iloc[i], 'mpaccdigit': rows_df['mPACCdigit'].iloc[i],
                            'mpacctrailsb': rows_df['mPACCtrailsB'].iloc[i], 'mmse':  rows_df['MMSE'].iloc[i], 'label': rows_df['DX'].iloc[i]}
 

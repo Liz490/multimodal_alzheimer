50ce9a5a2ea44ce26c7fb8837a4d6dd09d8ab285
Niklas Eisl <niklas.eisl@tum.de>
2022-12-14 09:47:38 +0100

    .

--- pkg/dataloader.py
+++ pkg/dataloader.py
@@ -4,6 +4,106 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import os
 import numpy as np
+import sys
+
+################################
+# TODO
+#
+# 1) always use all the features from tabular part? --> dropping by 'AGE' key not a nice solution
+
+
+# ds = pd.read_csv('/vol/chameleon/projects/adni/Adni_merged.csv')
+# filt  = ds['RID'] == 'sub-0021'
+# print(ds.loc[47, 'ICV'])
+
+# modalities: list
+
+# how to merge sessions together?
+# 1) check if len of modalities is 1 
+#  --> then we can drop all other rows, do a reindex and that's it
+# 2) choose a reference modality
+#  --> go iteratively over other modalities
+# for each session:
+# go over remaining modalities
+
+
+# goal: have a df with a subject id that holds the merged information from all modalities
+
+
+# __getitem__ returns a dict:
+# {'pet': None, 't1w': normalized tensor, 'tabular': innput feature vector}
+# if df[pet] is not None:
+#      dict['pet'] = ...
+#
+#     ....
+#
+# if df[tabular] is not None:  HERE WE NEED AN ADDITIONAL COLUMN THAT INDICATES WHETHER WE HAVE TAB DATA OR NOT
+#       - extract columns
+#       - fill in missing values
+#       dict['tabular'] = feature vector
+
+
+
+
+# tabular: list of all available feature cols in tabular
+
+# TODO check missing values in dataframe
+
+def merge_dfs(df_list):
+    ref_df = df_list[0]
+    #iterate over reference df
+
+class MultiModalDataset(Dataset):
+    
+    def __init__(self, 
+                path, 
+                modalities=['pet1451', 't1w', 'tabular'],
+                binary_classification=False,
+                days_threshold=180
+                ):
+        """
+        The constructor for MultiModalDataset class.
+  
+        Parameters:
+           path (str): The path to the csv file that holds the paths for all modalities and all labels.   
+        """
+        self.modalities = modalities
+        # read the dataframe with all modalities
+        self.entire_ds = pd.read_csv(path)
+        
+        self.df_list = []
+        # built reduced dataframe
+        self.ds_combined = pd.DataFrame()
+        if 'pet1451' in self.modalities:
+            self.df_pet = self.entire_ds.dropna(subset='path_pet1451')
+            self.df_list.append(self.df_pet)
+            self.ds_combined = pd.concat([self.ds_combined, self.df_pet], ignore_index=True)
+
+        if 't1w' in self.modalities:
+            self.df_anat = self.entire_ds.dropna(subset='path_anat')
+            self.df_list.append(self.df_anat)
+            self.ds_combined = pd.concat([self.ds_combined, self.df_anat], ignore_index=True)
+
+        if 'tabular' in self.modalities:
+            self.df_tab = self.entire_ds.dropna(subset='AGE')
+            self.df_list.append(self.df_tab)
+            self.ds_combined = pd.concat([self.ds_combined, self.df_tab], ignore_index=True)
+        
+        # remove MCI labels if desired
+        if binary_classification:
+            self.ds_combined = self.ds_combined[self.ds_combined['label'] != 'MCI']
+
+
+        # merge samples from different modalities with same label in a given timeintervals
+        if len(self.modalities) == 1:
+            self.ds = self.ds_combined
+        
+        else:
+            self.ds = merge_dfs(self.df_list)
+            
+
+    def __len__(self):
+        return len(self.ds)
 
 
 label_mapping = {'CN': 0, 'MCI': 1, 'Dementia': 2}
@@ -61,9 +161,10 @@ class PETAV1451Dataset(Dataset):
         return self.ds.loc[combined_idx]
 
 if __name__ == "__main__":
-    path = os.path.join(os.getcwd(), 'data/path_data_petav1451.csv')
-    dataset = PETAV1451Dataset(path=path, balanced=True)
-    #print(len(dataset))
+    path = os.path.join(os.getcwd(), 'data/train_path_data_labels.csv')
+    dataset = MultiModalDataset(path=path)
+    print(len(dataset))
+    
     # x, y = dataset[3]
     # print(x.shape)
     # print(y)

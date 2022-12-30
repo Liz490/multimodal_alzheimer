fe792d952fda780ee2ac447538e57e3b4459350c
lisa <lisa.schmierertum.de>
2022-12-30 12:32:52 +0100

    create data split considering all data samples

--- pkg/DataSplit.py
+++ pkg/DataSplit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
-
 import pandas as pd
 import json
 
+
 def split_tabular(path):
     """
-    Split tabular data by ID in a reproducable way and generate a .json file.
+    Splits tabular data by ID in a reproducible way and generates a .json file.
+    Args:
+        - path: path to file that contains all data samples
     """
-    adni = pd.read_csv(path, low_memory=False).dropna(subset=['DX'], inplace=True)
-    ids = adni['ID'].drop_duplicates()
+    adni = pd.read_csv(path, low_memory=False)
+    adni.dropna(subset=['DX'], inplace=True)
+    ids = adni['RID'].drop_duplicates()
     test = ids.sample(frac=0.1, random_state=3551)
     ids = ids.drop(test.index)
     val = ids.sample(frac=0.1, random_state=4381)
@@ -18,10 +21,11 @@ def split_tabular(path):
              'val': val.tolist(),
              'test': test.tolist()}
 
-    with open('data_set_split.json', 'w') as f:
+    with open('../data_set_split.json', 'w') as f:
         json.dump(split, f)
 
+
 if __name__ == "__main__":
-    PATH = './data/projects/adni/Adni_merged.csv'
+    PATH = '/vol/chameleon/projects/adni/Adni_merged.csv'
     split_tabular(PATH)
     print('Dataset split performed!')

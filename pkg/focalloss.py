b53e20778cfcafc569d6ef59bf2fdd36f979bcc2
Niklas Eisl <niklas.eisl@tum.de>
2023-01-04 11:42:32 +0100

    Included dimension arg for log_softmax function

--- pkg/focalloss.py
+++ pkg/focalloss.py
@@ -18,8 +18,8 @@ class FocalLoss(nn.Module):
             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
         target = target.view(-1,1)
-
-        logpt = F.log_softmax(input)
+        # change to original source code: add dimension argument
+        logpt = F.log_softmax(input,dim=1)
         logpt = logpt.gather(1,target)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())

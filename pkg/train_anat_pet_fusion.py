27f91c91709c330bcaaac3b83b5f832a2259e1b6
Niklas Eisl <niklas.eisl@tum.de>
2023-01-11 16:05:55 +0100

    Add 2 stage fusion model for MRI and PET

---
+++ pkg/train_anat_pet_fusion.py
@@ -0,0 +1,232 @@
+import os
+import torch
+from dataloader import MultiModalDataset
+from torch.utils.data import DataLoader
+from pkg.anat_cnn import Anat_CNN
+from pkg.pet_cnn import Small_PET_CNN
+from pkg.anat_pet_fusion import Anat_PET_CNN
+import optuna
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+import math
+from pkg.train_pet_cnn import ValidationLossTracker
+import sys
+
+LOG_DIRECTORY = 'lightning_logs'
+EXPERIMENT_NAME = 'optuna_pet_mri_fusion_two_class_var'
+EXPERIMENT_VERSION = None
+
+# PET and MRI models
+BASEPATH = os.getcwd()
+PATH_PET_CNN = os.path.join(BASEPATH, '/u/home/kos/adlm_adni/lightning_logs/optuna_two_class/version_144/checkpoints/epoch=49-step=49.ckpt')
+PATH_MRI_CNN = os.path.join(BASEPATH, '/u/home/kos/adlm_adni/lightning_logs/optuna_mri_two_class_var_resnet/version_301/checkpoints/epoch=49-step=49.ckpt')
+
+MODEL_PET = Small_PET_CNN.load_from_checkpoint(PATH_PET_CNN)
+MODEL_MRI = Anat_CNN.load_from_checkpoint(PATH_MRI_CNN)
+
+
+def options_list_to_dict(options: list) -> tuple[list, dict]:
+    """
+    Make options indexable by their string representation for optuna
+    compatibility. Optuna needs hashable types for sampling.
+
+    Args:
+        options: List of options
+
+    Returns:
+        A list of the options converted to strings and a dict of
+        the options with the strings as indices.
+    """
+    options_index = [str(o) for o in options]
+    options_dict = {i: o for i, o in zip(options_index, options)}
+    return options_index, options_dict
+
+
+def optuna_objective(trial):
+    """
+    Defines options for hyperparameters and selects hyperparameters based on
+    the options that performed well in previous runs. Starts training
+    afterwards and returns the value of the objective function.
+
+    See https://medium.com/optuna/using-optuna-to-optimize-pytorch-lightning-hyperparameters-d9e04a481585
+
+    Args:
+        trial: trial object, stores performance of hyperparameters
+        from previous runs
+
+    Returns:
+        Validation loss of last epoch
+    """
+
+    # Define fixed parameters
+    hparams = {
+        'early_stopping_patience': 5,
+        'max_epochs': 20,
+        'path_pet': PATH_PET_CNN,
+        'path_mri': PATH_MRI_CNN,
+        'n_classes': 2,
+        'gpu_id': 2,
+    }
+
+    # Define hyperparameter options and ranges
+    batch_size_options = [8, 16, 32, 64]
+    l2_options = [0, 1e-1, 1e-2, 1e-3]
+    lr_min = 1e-5
+    lr_max = 1e-2
+    gamma_options = [None, 1, 2, 5]
+    
+
+    # Let optuna select hyperparameters based on options defined above
+    hparams['lr'] = trial.suggest_float('lr', lr_min, lr_max, log=True)
+    hparams['batch_size'] = trial.suggest_categorical('batch_size',
+                                                      batch_size_options)
+    if hparams['batch_size'] >= 64:
+        # Increase early stopping patience and maximum number of epochs,
+        # if batch size is big
+        hparams['early_stopping_patience'] = 10
+        hparams['max_epochs'] = 50
+    hparams['l2_reg'] = trial.suggest_categorical('l2_reg', l2_options)
+    hparams['fl_gamma'] = trial.suggest_categorical('fl_gamma', gamma_options)
+
+    
+    # Train network
+    try:
+        val_loss = train_anat(hparams=hparams,
+                              model_pet=MODEL_PET,
+                              model_mri=MODEL_MRI,
+                              experiment_name=EXPERIMENT_NAME,
+                              experiment_version=EXPERIMENT_VERSION)
+        return val_loss
+    except torch.cuda.OutOfMemoryError:
+        print("Aborting run, not enough memory!")
+        return math.inf
+
+
+def train_anat(hparams, model_pet, model_mri, experiment_name='', experiment_version=None):
+    """
+    Train model for MRI data.
+
+    Args:
+        hparams: A dictionary of hyperparameters
+        experiment_name: Subdirectory for collection of runs in log directory
+        experiment_version: Name of this specific run
+
+    Returns:
+        Validation loss of last epoch
+    """
+    pl.seed_everything(15, workers=True)
+
+    assert hparams['n_classes'] == 2 or hparams['n_classes'] == 3
+    if hparams['n_classes'] == 2:
+        binary_classification=True
+    else:
+        binary_classification=False
+    
+    
+    # Setup datasets and dataloaders
+    trainpath = os.path.join(os.getcwd(), 'data/train_path_data_labels.csv')
+    valpath = os.path.join(os.getcwd(), 'data/val_path_data_labels.csv')
+
+    trainset = MultiModalDataset(
+        path=trainpath, 
+        modalities=['pet1451', 't1w'], 
+        normalize_mri={'per_scan_norm': 'min_max'},
+        normalize_pet={'mean': model_pet.hparams['norm_mean'], 'std': model_pet.hparams['norm_std']},
+        binary_classification=binary_classification, 
+        quantile=model_mri.hparams['norm_percentile'])
+    valset = MultiModalDataset(
+        path=valpath, 
+        modalities=['pet1451', 't1w'], 
+        normalize_mri={'per_scan_norm': 'min_max'},
+        normalize_pet = {'mean': model_pet.hparams['norm_mean'], 'std': model_pet.hparams['norm_std']},
+        binary_classification=binary_classification, 
+        quantile=model_mri.hparams['norm_percentile'])
+
+    trainloader = DataLoader(
+        trainset,
+        batch_size=hparams['batch_size'],
+        shuffle=True,
+        num_workers=32
+    )
+
+    valloader = DataLoader(
+        valset,
+        batch_size=hparams['batch_size'],
+        shuffle=False,
+        num_workers=32)
+
+    # Get class distribution of the trainset for weighted loss
+    _, weight_normalized = trainset.get_label_distribution()
+    hparams['loss_class_weights'] = 1 - weight_normalized
+
+    model = Anat_PET_CNN(hparams=hparams)
+
+    tb_logger = pl.loggers.TensorBoardLogger(
+        save_dir='lightning_logs',
+        name=experiment_name,
+        version=experiment_version)
+
+    val_loss_tracker = ValidationLossTracker()
+
+    trainer = pl.Trainer(
+        max_epochs=hparams['max_epochs'],
+        logger=tb_logger,
+        log_every_n_steps=5,
+        accelerator='gpu',
+        devices=1,
+        callbacks=[
+            EarlyStopping(
+                monitor='val_loss',
+                mode='min',
+                patience=hparams['early_stopping_patience']
+            ),
+            val_loss_tracker
+        ]
+    )
+
+    trainer.fit(model, trainloader, valloader)
+    return val_loss_tracker.val_loss[-1]
+
+
+def optuna_optimization():
+    """
+    Create an optuna study and minimize the objective
+    """
+    study = optuna.create_study(direction="minimize")
+    seconds_per_day = 24 * 60 * 60
+    study.optimize(optuna_objective, n_trials=300, timeout=7*seconds_per_day)
+
+
+if __name__ == '__main__':
+    #####################
+    # Uncomment and comment the rest for optuna optimization
+    optuna_optimization()
+    #####################
+
+    # # Experimental hyperparameters
+    # hparams = {
+    #     'early_stopping_patience': 5,
+    #     'max_epochs': 20,
+    #     'norm_mean_train': 413.6510,
+    #     'norm_std_train': 918.5371,
+    #     'norm_mean_val': 418.4120,
+    #     'norm_std_val': 830.2466,
+    #     'n_classes': 2,
+    #     'lr': 1e-4,
+    #     'batch_size': 64,
+    #     'fl_gamma': 2,
+    #     # 'conv_out': [],
+    #     # 'filter_size': [5, 5],
+    #     'lr_pretrained': 1e-5,
+    #     'batchnorm_begin': True,
+    #     # 'batchnorm_conv': True,
+    #     'batchnorm_dense': True,
+    #     'l2_reg': 1e-2,
+    #     # 'linear_out': [256, 256, 256],
+    #     'linear_out': [],
+    #     'norm_percentile': 0.99,
+    #     'resnet_depth': 18,
+    #     'gpu_id': 6,
+    # }
+
+    # train(hparams)

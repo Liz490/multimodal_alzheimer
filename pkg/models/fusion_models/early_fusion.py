c8bcd4162651d45c431895e7d0779e2765e9918e
Niklas Eisl <niklas.eisl@tum.de>
2023-01-24 22:35:06 +0100

    Add scripts for early fusion model

---
+++ pkg/models/fusion_models/early_fusion.py
@@ -0,0 +1,244 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from torchmetrics.classification import MulticlassF1Score
+import torchvision
+import torchmetrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import io
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from pkg.loss_functions.focalloss import FocalLoss
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+class IntHandler:
+    """
+    See https://stackoverflow.com/a/73388839
+    """
+
+    def legend_artist(self, legend, orig_handle, fontsize, handlebox):
+        x0, y0 = handlebox.xdescent, handlebox.ydescent
+        text = plt.matplotlib.text.Text(x0, y0, str(orig_handle))
+        handlebox.add_artist(text)
+        return text
+
+
+class PET_MRI_EF(pl.LightningModule):
+
+    def __init__(self, hparams, gpu_id=None):
+        super().__init__()
+        self.save_hyperparameters(hparams)
+        if hparams["n_classes"] == 3:
+            self.label_ind_by_names = {'CN': 0, 'MCI': 1, 'AD': 2}
+        else:
+            self.label_ind_by_names = {'CN': 0, 'AD': 1}
+
+        modules = nn.ModuleList()
+
+        # Convolutional Block
+        # n * (Conv, [Batchnorm], Activation, [Dropout], MaxPool)
+        # we need two input channels - one for PET and one for MRI
+        n_in = 2
+        for n_out, filter_size in zip(self.hparams["conv_out"],
+                                      self.hparams["filter_size"]):
+            modules.append(nn.Conv3d(n_in, n_out, filter_size, padding="same"))
+            if "batchnorm" in self.hparams and self.hparams["batchnorm"]:
+                modules.append(nn.BatchNorm3d(n_out))
+            modules.append(nn.ReLU())
+            modules.append(nn.MaxPool3d(2))
+            if "dropout_conv_p" in self.hparams:
+                modules.append(nn.Dropout(p=self.hparams["dropout_conv_p"]))
+            n_in = n_out
+
+        # Connector Block
+        modules.append(nn.AdaptiveAvgPool3d(1))
+        modules.append(nn.Flatten())
+
+        # Dense Block
+        # n * ([Dropout], Linear)
+        if "linear_out" in self.hparams and self.hparams["linear_out"]:
+            n_out = self.hparams["linear_out"]
+            if "dropout_dense_p" in self.hparams:
+                modules.append(
+                    nn.Dropout(p=self.hparams["dropout_dense_p"]))
+            modules.append(nn.Linear(n_in, n_out))
+            modules.append(nn.ReLU())
+        modules.append(nn.Linear(n_out, self.hparams["n_classes"]))
+
+        self.model = nn.Sequential(*modules)
+
+        if 'fl_gamma' in hparams and hparams['fl_gamma']:
+            self.criterion = FocalLoss(gamma=self.hparams['fl_gamma'])
+        else:
+            self.criterion = nn.CrossEntropyLoss(
+                weight=hparams['loss_class_weights'])
+
+        self.criterion = nn.CrossEntropyLoss(
+            weight=hparams['loss_class_weights'])
+        self.f1_score_train = MulticlassF1Score(
+            num_classes=self.hparams["n_classes"], average='macro')
+        self.f1_score_train_per_class = MulticlassF1Score(
+            num_classes=self.hparams["n_classes"], average='none')
+        self.f1_score_val = MulticlassF1Score(
+            num_classes=self.hparams["n_classes"], average='macro')
+        self.f1_score_val_per_class = MulticlassF1Score(
+            num_classes=self.hparams["n_classes"], average='none')
+
+    def forward(self, x):
+        """
+        Forward pass of the convolutional neural network. Should not be called
+        manually but by calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+        return self.model(x)
+
+    @property
+    def is_cuda(self):
+        """
+        Check if model parameters are allocated on the GPU.
+        """
+        return next(self.parameters()).is_cuda
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self, path)
+
+    def general_step(self, batch, batch_idx, mode):
+        x_pet = batch['pet1451']
+        x_mri = batch['mri']
+        y = batch['label']
+        x = torch.stack((x_pet, x_mri), dim=1)
+        x = x.to(dtype=torch.float32)
+        y_hat = self.forward(x).to(dtype=torch.double)
+
+        loss = self.criterion(y_hat, y)
+        if mode != 'pred':
+            self.log(mode + '_loss', loss, on_step=True)
+        if mode == 'val':
+            self.f1_score_val(y_hat, y)
+            self.f1_score_val_per_class(y_hat, y)
+        elif mode == 'train':
+            self.f1_score_train(y_hat, y)
+            self.f1_score_train_per_class(y_hat, y)
+        return {'loss': loss, 'outputs': y_hat, 'labels': y}
+
+    def training_step(self, batch, batch_idx):
+        return self.general_step(batch, batch_idx, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self.general_step(batch, batch_idx, "val")
+
+    def predict_step(self, batch, batch_idx):
+        return self.general_step(batch, batch_idx, "pred")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams['lr'])
+        if self.hparams['reduce_factor_lr_schedule']:
+            scheduler = ReduceLROnPlateau(optimizer, factor=self.hparams['reduce_factor_lr_schedule'])
+            return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss_epoch"}
+            #return [optimizer], [scheduler]
+        else:
+            return optimizer
+
+    def training_epoch_end(self, training_step_outputs):
+        avg_loss = torch.stack([x['loss']
+                               for x in training_step_outputs]).mean()
+        f1_epoch = self.f1_score_train.compute()
+        f1_epoch_per_class = self.f1_score_train_per_class.compute()
+        self.f1_score_train.reset()
+        self.f1_score_train_per_class.reset()
+
+        log_dict = {
+            'train_loss_epoch': avg_loss,
+            'train_f1_epoch': f1_epoch,
+            'step': float(self.current_epoch)
+        }
+        for i in range(self.hparams["n_classes"]):
+            log_dict[f"train_f1_epoch_class_{i}"] = f1_epoch_per_class[i]
+        self.log_dict(log_dict)
+
+        im = self.generate_confusion_matrix(training_step_outputs)
+        self.logger.experiment.add_image(
+            "train_confusion_matrix", im, global_step=self.current_epoch)
+
+    def validation_epoch_end(self, validation_step_outputs):
+        avg_loss = torch.stack([x['loss']
+                               for x in validation_step_outputs]).mean()
+        f1_epoch = self.f1_score_val.compute()
+        f1_epoch_per_class = self.f1_score_val_per_class.compute()
+        self.f1_score_val.reset()
+        self.f1_score_val_per_class.reset()
+
+        # current_lr = optimizer.param_groups[0]['lr']
+        log_dict = {
+            'val_loss_epoch': avg_loss,
+            'val_f1_epoch': f1_epoch,
+            'step': float(self.current_epoch)
+        }
+        for i in range(self.hparams["n_classes"]):
+            log_dict[f"val_f1_epoch_class_{i}"] = f1_epoch_per_class[i]
+        self.log_dict(log_dict)
+
+        im = self.generate_confusion_matrix(validation_step_outputs)
+        self.logger.experiment.add_image(
+            "val_confusion_matrix", im, global_step=self.current_epoch)
+
+    def generate_confusion_matrix(self, outs):
+        """
+        See https://stackoverflow.com/a/73388839
+        """
+
+        outputs = torch.cat([tmp['outputs'] for tmp in outs])
+        labels = torch.cat([tmp['labels'] for tmp in outs])
+
+        confusion = torchmetrics.ConfusionMatrix(
+            num_classes=self.hparams["n_classes"]).to(outputs.get_device())
+        confusion(outputs, labels)
+        computed_confusion = confusion.compute().detach().cpu().numpy().astype(int)
+
+        # confusion matrix
+        df_cm = pd.DataFrame(
+            computed_confusion,
+            index=self.label_ind_by_names.values(),
+            columns=self.label_ind_by_names.values(),
+        )
+
+        fig, ax = plt.subplots(figsize=(10, 5))
+        fig.subplots_adjust(left=0.05, right=.65)
+        sns.set(font_scale=1.2)
+        sns.heatmap(df_cm, annot=True, annot_kws={
+                    "size": 16}, fmt='d', ax=ax, cmap='crest')
+        ax.legend(
+            self.label_ind_by_names.values(),
+            self.label_ind_by_names.keys(),
+            handler_map={int: IntHandler()},
+            loc='upper left',
+            bbox_to_anchor=(1.2, 1)
+        )
+        buf = io.BytesIO()
+
+        plt.savefig(buf, format='jpeg', bbox_inches='tight')
+        plt.close('all')
+        buf.seek(0)
+        with Image.open(buf) as im:
+            return torchvision.transforms.ToTensor()(im)
+
+
+class Random_Benchmark_All_CN(PET_MRI_EF):
+    def forward(self, x):
+        y_hat = torch.zeros_like(super().forward(x))
+        y_hat[..., 0] = 1
+        y_hat[..., 1:] = 0
+        return y_hat
---
+++ pkg/models/fusion_models/train_early_fusion.py
@@ -0,0 +1,273 @@
+import os
+import torch
+import math
+from pathlib import Path
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, ToTensor, Normalize
+from pkg.models.fusion_models.early_fusion import PET_MRI_EF, Random_Benchmark_All_CN
+import optuna
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import Callback, LearningRateMonitor, ModelCheckpoint
+from pkg.utils.dataloader import MultiModalDataset
+
+LOG_DIRECTORY = 'lightning_logs'
+EXPERIMENT_NAME = 'early_fusion'
+EXPERIMENT_VERSION = None
+
+
+
+class ValidationLossTracker(Callback):
+    """
+    Tracks validation loss per epoch across epochs
+
+    See
+    https://stackoverflow.com/questions/69276961/how-to-extract-loss-and-accuracy-from-logger-by-each-epoch-in-pytorch-lightning"""
+
+    def __init__(self):
+        self.val_loss = []
+
+    def on_validation_epoch_end(self, trainer, module):
+        val_loss = trainer.logged_metrics['val_loss_epoch']
+        self.val_loss.append(val_loss)
+
+
+def optuna_just_sampling(trial):
+    """
+    See https://medium.com/optuna/using-optuna-to-optimize-pytorch-lightning-hyperparameters-d9e04a481585
+    """
+    # Define hyperparameter options
+    lr_min = 5e-6
+    lr_max = 1e-3
+    batch_size_options = [8, 16, 32, 64]
+    gamma_options = [None, 1, 2, 5]
+    q_options = [0.95, 0.98, 0.99, 1]
+
+    # Dynamic generation of conv_out options
+    conv_out_first_options = [8, 16, 32]
+    n_conv_options = [3, 4]
+    conv_out_options = []
+    for x in conv_out_first_options:
+        for n in n_conv_options:
+            layers = tuple([2**i * x for i in range(n)])
+            conv_out_options.append(layers)
+
+    # Make options indexable by their string representation for optuna compatibility
+    conv_out_options_index = [str(o) for o in conv_out_options]
+    conv_out_options_dict = {i: o for i, o in zip(conv_out_options_index,
+                                                  conv_out_options)}
+
+    filter_size_options = [(5, 5, 3, 3),
+                           (7, 5, 3, 3),
+                           (5, 5, 5, 3),
+                           (3, 3, 3, 3)]
+
+    # Make options indexable by their string representation for optuna compatibility
+    filter_size_options_index = [str(o) for o in filter_size_options]
+    filter_size_options_dict = {i: o for i, o in zip(filter_size_options_index,
+                                                     filter_size_options)}
+
+    linear_out_options = [64] #[False, 32, 64, 128]
+    dropout_conv_min = 0.05
+    dropout_conv_max = 0.2
+    dropout_dense_min = 0.2
+    dropout_dense_max = 0.5
+    batchnorm_options = [True, False]
+
+    # Let optuna configure hyperparameters based on options defined above
+    hparams = {
+        'early_stopping_patience': 5,
+        'max_epochs': 20,
+        'norm_mean': 0.5145,
+        'norm_std': 0.5383,
+        'reduce_factor_lr_schedule': None,
+        'n_classes': 2
+    }
+    hparams['lr'] = trial.suggest_float(
+        'learning_rate', lr_min, lr_max, log=True)
+    conv_out_idx = trial.suggest_categorical('conv_out',
+                                             conv_out_options_index)
+    hparams['conv_out'] = conv_out_options_dict[conv_out_idx]
+    filter_size_idx = trial.suggest_categorical(
+        'filter_size', filter_size_options_index)
+    hparams['filter_size'] = filter_size_options_dict[filter_size_idx]
+    hparams['batchnorm'] = trial.suggest_categorical('batchnorm',
+                                                     batchnorm_options)
+    hparams['linear_out'] = trial.suggest_categorical(
+        'linear_out', linear_out_options)
+    hparams['batch_size'] = trial.suggest_categorical('batch_size',
+                                                      batch_size_options)
+    if hparams['batch_size'] >= 64:
+        hparams['early_stopping_patience'] = 10
+        hparams['max_epochs'] = 50
+    dropout_conv = trial.suggest_categorical('dropout_conv', (True, False))
+    dropout_dense = trial.suggest_categorical('dropout_dense', (True, False))
+    if dropout_conv:
+        hparams['dropout_conv_p'] = trial.suggest_float(
+            'dropout_conv_p', dropout_conv_min, dropout_conv_max)
+    if dropout_dense:
+        hparams['dropout_dense'] = trial.suggest_float(
+            'dropout_dense_p', dropout_dense_min, dropout_dense_max)
+    hparams['norm_percentile'] = trial.suggest_categorical('norm_percentile',
+                                                           q_options)
+    hparams['fl_gamma'] = trial.suggest_categorical('fl_gamma', gamma_options)
+    # Train network
+    try:
+        val_loss = train(hparams,
+                         experiment_name=EXPERIMENT_NAME,
+                         experiment_version=EXPERIMENT_VERSION)
+        return val_loss
+    except torch.cuda.OutOfMemoryError:
+        print("Aborting run, not enough memory!")
+        return math.inf
+
+
+def train(hparams,
+          experiment_name='',
+          experiment_version=None,
+          trial=None):
+    pl.seed_everything(5, workers=True)
+
+    # CALLBACKS
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    # checkpoint_callback = ModelCheckpoint(
+    # save_top_k=1,
+    # monitor="val_f1_epoch",
+    # mode="max",
+    # dirpath=experiment_name,
+    # filename="best-run-{epoch:02d}-{val_f1_epoch:.4f}",
+# )
+
+    # TRANSFORMS
+    normalization_pet = {'mean': hparams['norm_mean'], 'std': hparams['norm_std']}
+
+    assert hparams['n_classes'] == 2 or hparams['n_classes'] == 3
+    if hparams['n_classes'] == 2:
+        binary_classification=True
+    else:
+        binary_classification=False
+
+    # DATASET AND DATALOADER
+    trainpath = os.path.join(os.getcwd(), 'data/train_path_data_labels.csv')
+    valpath = os.path.join(os.getcwd(), 'data/val_path_data_labels.csv')
+
+    remove_mci = hparams["n_classes"] == 2
+    
+    trainset = MultiModalDataset(
+        path=trainpath, 
+        modalities=['pet1451', 't1w'], 
+        normalize_mri={'per_scan_norm': 'min_max'},
+        normalize_pet={'mean': hparams['norm_mean'], 'std': hparams['norm_std']},
+        binary_classification=binary_classification, 
+        quantile=hparams['norm_percentile'])
+    valset = MultiModalDataset(
+        path=valpath, 
+        modalities=['pet1451', 't1w'], 
+        normalize_mri={'per_scan_norm': 'min_max'},
+        normalize_pet = {'mean': hparams['norm_mean'], 'std': hparams['norm_std']},
+        binary_classification=binary_classification, 
+        quantile=hparams['norm_percentile'])
+
+
+
+    trainloader = DataLoader(
+        trainset,
+        batch_size=hparams['batch_size'],
+        shuffle=True,
+        num_workers=32
+    )
+    valloader = DataLoader(
+        valset,
+        batch_size=hparams['batch_size'],
+        num_workers=32)
+
+    _, weight_normalized = trainset.get_label_distribution()
+    hparams['loss_class_weights'] = 1 - weight_normalized
+    hparams['loss_class_weights_human_readable'] = hparams['loss_class_weights'].tolist()  # original hparam is a Tensor that isn't stored in human readable format
+
+    # model = Random_Benchmark(hparams=hparams)
+    model = PET_MRI_EF(hparams=hparams)
+
+    tb_logger = pl.loggers.TensorBoardLogger(
+        save_dir='lightning_logs',
+        name=experiment_name,
+        version=experiment_version)
+
+    val_loss_tracker = ValidationLossTracker()
+    trainer = pl.Trainer(
+        max_epochs=hparams['max_epochs'],
+        logger=tb_logger,
+        log_every_n_steps=5,
+        accelerator='gpu',
+        devices=1,
+        callbacks=[
+            EarlyStopping(
+                monitor='val_loss_epoch',
+                mode='min',
+                patience=hparams['early_stopping_patience']),
+            val_loss_tracker,
+            lr_monitor,
+            ModelCheckpoint(monitor='val_loss_epoch')
+        ]
+    )
+    
+    trainer.fit(model, trainloader, valloader)
+    return val_loss_tracker.val_loss[-1]
+
+
+def optuna_optimization():
+    study = optuna.create_study(direction="minimize")
+    study.optimize(optuna_just_sampling, n_trials=300, timeout=86400)
+
+
+if __name__ == '__main__':
+    #####################
+    # Uncomment and comment the rest for optuna optimization
+    # optuna_optimization()
+    #####################
+
+    # Experimental
+    # hparams = {
+    #     'early_stopping_patience': 5,
+    #     'max_epochs': 20,
+    #     'norm_mean': 0.5145,
+    #     'norm_std': 0.5383
+    # }
+
+    # hparams['lr'] = 0.00006
+    # hparams['batch_size'] = 8
+    # hparams['conv_out'] = [8, 16, 32, 64]
+    # hparams['filter_size'] = [5, 5, 5, 3]  # More filters for more layers!
+    # hparams['batchnorm'] = True
+    # # hparams['dropout_conv_p'] = 0.1
+    # # hparams['dropout_dense_p'] = 0.5
+    # hparams['linear_out'] = 64
+    # hparams["n_classes"] = 2
+
+
+    # Best two class v26:
+    hparams = {
+        'early_stopping_patience': 30,
+        'max_epochs': 300,
+        'norm_mean': 0.5145,
+        'norm_std': 0.5383,
+        'lr': 1.92419735361303e-05,
+        'batch_size': 64,
+        'conv_out': [16, 32, 64, 128],
+        'filter_size': [3, 3, 3, 3],
+        'batchnorm': False,
+        'n_classes': 2,
+        'linear_out': 64,
+        'dropout_dense': 0.3188563832255344,
+        'fl_gamma': 5,
+        'batchnorm': True,
+        'reduce_factor_lr_schedule': 0.1,
+        'norm_percentile': 0.98
+
+    }
+
+    
+
+    train(hparams, experiment_name='best_runs', experiment_version='early_fusion_2_class')
+    # # train(hparams)
+    # # TODO rerun with MCI samples

a7fe9de01861eb0028cba285191686b6cedb1a7d
Simon Koch <simon.leo.koch@tum.de>
2023-02-05 15:48:29 +0100

    Add pytorch lightning wrapper for tabular model

--- pkg/inference/test_tab.py
+++ pkg/inference/test_tab.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import torch
+
 from pkg.models.tabular_models.tabular_pl_wrapper import Tabular_Model
 from pkg.utils.dataloader import MultiModalDataset
 from pkg.utils.test import test
@@ -14,15 +16,30 @@ def tab_testset(paths: dict, binary_classification: bool = True):
     return testset
 
 
-def tab_model(paths: dict, binary_classification: bool = True):
+def tab_model(binary_classification: bool = True):
     if binary_classification:
-        model = Tabular_Model.load_from_checkpoint(
-            paths['tab_2_class']
-        )
+        hparams = {
+            'n_classes': 2,
+            'ensemble_size': 4,
+            'batch_size': 64,
+            'loss_class_weights': torch.tensor([
+                0.20314960629921264,
+                0.7968503937007874
+            ], dtype=torch.double)
+        }
+        model = Tabular_Model(hparams)
     else:
-        model = Tabular_Model.load_from_checkpoint(
-            paths['tab_3_class']
-        )
+        hparams = {
+            'n_classes': 3,
+            'ensemble_size': 4,
+            'batch_size': 64,
+            'loss_class_weights': torch.tensor([
+                0.4651162790697675,
+                0.6712473572938689,
+                0.8636363636363636
+            ], dtype=torch.double)
+        }
+        model = Tabular_Model(hparams)
     return model
 
 
@@ -31,12 +48,12 @@ if __name__ == '__main__':
 
     # Two class
     testset = tab_testset(paths, binary_classification=True)
-    model = tab_model(paths, binary_classification=True)
+    model = tab_model(binary_classification=True)
     experiment_name = 'test_set_tab_2_class'
     test(testset, model, experiment_name)
 
     # Three class
     testset = tab_testset(paths, binary_classification=False)
-    model = tab_model(paths, binary_classification=False)
+    model = tab_model(binary_classification=False)
     experiment_name = 'test_set_tab_3_class'
     test(testset, model, experiment_name)
